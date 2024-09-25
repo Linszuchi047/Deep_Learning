e78cb50e03304b13c284a1cf850dbb3927dafced
Linszuchi047 <l109704020.mg09@nycu.edu.tw>
2024-09-25 22:41:09 +0800

    change

---
+++ HW1/model.py
@@ -0,0 +1,8 @@
+import torch.nn as nn
+
+class ClassificationModel(nn.Module):
+    def __init__(self):
+        # TODO
+
+    def forward(self, x):
+        # TODO
---
+++ HW1/train.py
@@ -0,0 +1,7 @@
+from model import ClassificationModel
+
+def train():
+    # TODO
+
+if __name__ == "__main__":
+    train()
